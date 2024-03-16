feff59fac3af636c0ae4f9b207ff89c2977aa51e
Siddhraj <siddhraj.sisodiya7@gmail.com>
2024-03-16 16:37:59 +0530

    added wallpaper channel for android

--- android/app/src/main/kotlin/com/example/minimal_launcher/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/minimal_launcher/MainActivity.kt
@@ -1,5 +1,35 @@
 package com.example.minimal_launcher
 
+import android.app.WallpaperManager
+import android.content.Context
+import android.os.Build
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity()
+class MainActivity : FlutterActivity() {
+    companion object {
+        private const val CHANNEL = "com.example.wallpaper"
+    }
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getWallpaperPath") {
+                val wallpaperPath = getWallpaperPath()
+                result.success(wallpaperPath)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun getWallpaperPath(): String? {
+        val context: Context = applicationContext
+        val wallpaperManager = WallpaperManager.getInstance(context)
+        print(wallpaperManager)
+        return ""
+    }
+}
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,61 @@
+import 'dart:async';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(MyApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  static const platform = MethodChannel('com.example.wallpaper');
+  String wallpaperPath = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _getWallpaperPath();
+  }
+
+  Future<void> _getWallpaperPath() async {
+    try {
+      final String result = await platform.invokeMethod('getWallpaperPath');
+      setState(() {
+        wallpaperPath = result;
+      });
+    } on PlatformException catch (e) {
+      print("Failed to get wallpaper: '${e.message}'.");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Wallpaper Example'),
+      ),
+      body: Center(
+        child: wallpaperPath.isNotEmpty
+            ? Image.file(
+                File(wallpaperPath),
+                fit: BoxFit.cover,
+              )
+            : CircularProgressIndicator(),
       ),
     );
   }
